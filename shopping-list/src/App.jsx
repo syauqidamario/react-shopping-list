1d62a634cef0b39cb6b14cc3146260e70a36be11
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-10-04 23:09:18 +0700

    On progress: Preparing new items, lifting state up, managing items, putting checklist on items.

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -22,21 +22,40 @@ const groceryItems = [
 ];
 
 export default function App() {
+
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item)
+  {
+    setItems([...items, item]);
+  }
+
+  function handleDeleteItem(id)
+  {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id)
+  {
+    setItems((items) => items.map((item) => (item.id === id ? {...item, checked: !item.
+    checked}:{item})));
+  }
+
   return (
   <div className="app">
     <Header/>
-    <Form/>
-    <GroceryList/>
+    <Form onAddItem={handleAddItem}/>
+    <GroceryList item={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
     <Footer/>
   </div>
   );
 }
 
 function Header(){
-  return <h1>My Shopping List 📝</h1>
+  return <h1>My Shopping List📝</h1>
 }
 
-function Form(){
+function Form({ onAddItem }){
 
   const [name, setName] = useState('');
   const [quantity, setQuantity] = useState(1);
@@ -44,10 +63,18 @@ function Form(){
   function handleSubmit(e)
   {
     e.preventDefault();
+
+    if(!name) return;
     
 
     const newItem = { name, quantity, checked:false, id: Date.now() };
+
+    onAddItem(newItem);
+
     console.log(newItem);
+
+    setName('');
+    setQuantity(1);
   }
 
   const quantityNo = [...Array(40)].map((_, i) => (
@@ -60,7 +87,7 @@ function Form(){
     <form className="add-form" onSubmit={handleSubmit}>
         <h3>What are we buying today?</h3>
         <div>
-          <select value={quantity} onChange={(e)=>setQuantity(e.target.value)}>{quantityNo}</select>
+          <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>{quantityNo}</select>
           <input type="text" placeholder="Item names" value={name} onChange={(e) => setName(e.target.value)}/>
         </div>
         <button>Add</button>
@@ -68,37 +95,37 @@ function Form(){
   );
 }
 
-function GroceryList(){
+function GroceryList({items, onDeleteItem, onToggleItem}){
   return (
     <>
   
     <div className="list">
         <ul>
-          {groceryItems.map((item) => (
-            <Item item={item} key ={item.id} />
+          {items.map((item) => (
+            <Item item={item} key ={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
           ))}
         </ul>
       </div>
       <div className="actions">
         <select>
-          <option value="input">Urutkan berdasarkan urutan input</option>
-          <option value="name">Urutkan berdasarkan nama barang</option>
-          <option value="checked">Urutkan berdasarkan ceklis</option>
+          <option value="input">Arrange based on input arrangement</option>
+          <option value="name">Arrange based on item names</option>
+          <option value="checked">Arrange based on checklist</option>
         </select>
-        <button>Bersihkan Daftar</button>
+        <button>Clear List</button>
       </div>
       </>
   );
 }
 
-function Item({item}){
+function Item({item, onDeleteItem, onToggleItem }){
   return(
     <li key={item.id}>
-             <input type="checkbox" />
+             <input type="checkbox" checked={item.checked} onChange={()=>onToggleItem(item.id)} />
              <span style={ item.checked ? { textDecoration:'line-through' }: {}}>
               {item.quantity} {item.name}
               </span>
-             <button>&times;</button>
+             <button onClick={()=>onDeleteItem(item.id)}>&times;</button>
            </li>
   );
 }
